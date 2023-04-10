fc44f8f3ae4d3441e564b2534cd63c9e747c6a29
Kim, gaeun <0107162a@gmail.com>
2023-04-10 17:58:23 +0900

    4.10

--- walk/lib/main.dart
+++ walk/lib/main.dart
@@ -1,34 +1,70 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:shake/shake.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const WalkApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class WalkApp extends StatelessWidget {
+  const WalkApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(),
+    return MaterialApp(
+      title: 'Mountain Dew',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const WalkWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class WalkWidget extends StatefulWidget {
+  const WalkWidget({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<WalkWidget> createState() => _WalkWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _WalkWidgetState extends State<WalkWidget> {
   int walk = 0;
   ShakeDetector? shakeDetector;
+  late Timer timer;
+  int totaltime = 0;
+
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds);
+    return duration.toString().split(".").first.substring(2, 7);
+  }
+
+  void Time(Timer timer) {
+    setState(() {
+      totaltime = totaltime + 1;
+    });
+  }
 
   void onPhoneShake() {
     setState(() {
       walk = walk + 1;
+    });
+  }
+
+  void startWalk() {
+    setState(() {
+      timer = Timer.periodic(
+        const Duration(seconds: 1),
+        Time,
+      );
+    });
+  }
+
+  void stopWalk() {
+    setState(() {
+      timer.cancel();
     });
   }
 
@@ -39,37 +75,112 @@ class _MyHomePageState extends State<MyHomePage> {
     shakeDetector = ShakeDetector.autoStart(
       // 흔들기 감지 즉시 시작
       shakeSlopTimeMS: 1, // 감지 주기
-      shakeThresholdGravity: 10000, // 감지 민감도
+      shakeThresholdGravity: 2.7, // 감지 민감도
       onPhoneShake: onPhoneShake, // 감지 후 실행할 함수
+    );
+    shakeDetector = ShakeDetector.autoStart(
+      shakeThresholdGravity: 2.7, // 감지 민감도
+      onPhoneShake: startWalk, // 감지 후 실행할 함수
     );
   }
 
   @override
   void dispose() {
     shakeDetector!.stopListening(); // 흔들기 감지 중지
+    stopWalk();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: CircleAvatar(
-        radius: 140.0,
-        backgroundColor: Colors.green[300],
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              '$walk',
-              style: const TextStyle(
-                fontSize: 70.0,
-                color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 100,
+                    ),
+                    CircularStepProgressIndicator(
+                      totalSteps: 100,
+                      currentStep: (0.01 * walk).floor(),
+                      stepSize: 30,
+                      selectedColor: Colors.green[200],
+                      unselectedColor: Colors.grey[200],
+                      padding: 0,
+                      width: 250,
+                      height: 250,
+                      selectedStepSize: 30,
+                      roundedCap: (_, __) => false,
+                      child: Center(
+                        child: Text(
+                          '$walk',
+                          style: const TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Column(
+                            children: [
+                              Icon(
+                                Icons.timeline,
+                                size: 60,
+                                color: Color.fromARGB(255, 48, 158, 248),
+                              ),
+                              Text('거리'),
+                              Text('3.92KM'),
+                            ],
+                          ),
+                          const SizedBox(width: 50),
+                          const Column(
+                            children: [
+                              Icon(
+                                Icons.local_fire_department,
+                                size: 60,
+                                color: Color.fromARGB(255, 236, 83, 18),
+                              ),
+                              Text('칼로리'),
+                              Text('1,353KCAL'),
+                            ],
+                          ),
+                          const SizedBox(width: 50),
+                          Column(
+                            children: [
+                              const Icon(
+                                Icons.timer,
+                                size: 60,
+                                color: Color.fromARGB(255, 255, 208, 66),
+                              ),
+                              const Text('시간'),
+                              Text(
+                                format(totaltime),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
         ),
       ),
-    ));
+    );
   }
 }
